3225da178afbdbb1e1d6bf3356e91965f9f21f18
Tom Evon <evon@kogosoftwarellc.com>
2017-06-20 15:31:19 -0700

    added repo css and js

---
+++ repoFinderJS.js
@@ -0,0 +1,27 @@
+var repoApp = angular.module('repoFinderApp' []);
+
+repoApp.controller('repoCtrl'), function ($scope) {
+	$scope.getRep = function (searchInput) {
+		
+	}
+
+}
+
+
+// var calcApp = angular.module('calcApp', []);
+
+// calcApp.controller('calcCtrl', function ($scope) {
+//     $scope.leftOperand = "";
+//     $scope.operator = "";
+//     $scope.rightOperand = "";
+//     $scope.answer = "";
+
+
+//     $scope.setOperand = function (operandEntered) {
+//         if ($scope.operator){
+//             $scope.rightOperand += operandEntered;
+//         }
+//         else {
+//             $scope.leftOperand += operandEntered;
+//         };
+//     };
