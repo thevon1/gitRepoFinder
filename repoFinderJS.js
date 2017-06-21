6d5b50a00c2dcdfb684d24d035034317cf3643f5
Tom Evon <evon@kogosoftwarellc.com>
2017-06-21 14:05:56 -0700

    add some css, added shell for user's input choice of repo, having error with links for the loading bar, noted them

--- repoFinderJS.js
+++ repoFinderJS.js
@@ -1,11 +1,33 @@
-var repoApp = angular.module('repoFinderApp' []);
+var repoApp = angular.module('repoFinderApp', ['angular-loading-bar']);
 
-repoApp.controller('repoCtrl'), function ($scope) {
-	$scope.getRep = function (searchInput) {
-		
+repoApp.controller('repoCtrl', function ($scope) {
+	$scope.searchInput = function (reqSearchItem) {
+		$scope.searchItem = reqSearchItem;
 	}
 
-}
+	$scope.fetchJSON = function (someSearchName) {
+		// https://api.github.com/repositories
+	}
+
+});
+
+
+// *************
+// function requestListener(){
+//      var file = JSON.parse(this.responseText);
+//      console.log(file[0]);
+     
+    
+// }
+
+// var openRequest = new XMLHttpRequest();
+
+// openRequest.addEventListener("load", requestListener);
+// openRequest.open("GET", "https://api.github.com/orgs/kogosoftwarellc/repos");
+// openRequest.send();
+
+// ******************
+
 
 
 // var calcApp = angular.module('calcApp', []);
