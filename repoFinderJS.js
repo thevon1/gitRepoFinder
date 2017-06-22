31078127a90bebe695782bbf4170524b8b365669
Tom Evon <evon@kogosoftwarellc.com>
2017-06-21 19:34:23 -0700

    added in JSON request, xmlhttp request, fixed errors for links by downloading the node_modules for angular loading bar

--- repoFinderJS.js
+++ repoFinderJS.js
@@ -1,49 +1,16 @@
 var repoApp = angular.module('repoFinderApp', ['angular-loading-bar']);
 
 repoApp.controller('repoCtrl', function ($scope) {
-	$scope.searchInput = function (reqSearchItem) {
-		$scope.searchItem = reqSearchItem;
-	}
+	
+	function reqListener () {
+  		document.getElementById("repo-return-box").innerHTML = JSON.parse(this.responseText);
+}
 
-	$scope.fetchJSON = function (someSearchName) {
-		// https://api.github.com/repositories
-	}
+var openReq = new XMLHttpRequest();
+openReq.addEventListener("load", reqListener);
+openReq.open("GET", "https://api.github.com/repositories");
+openReq.send();
 
 });
 
 
-// *************
-// function requestListener(){
-//      var file = JSON.parse(this.responseText);
-//      console.log(file[0]);
-     
-    
-// }
-
-// var openRequest = new XMLHttpRequest();
-
-// openRequest.addEventListener("load", requestListener);
-// openRequest.open("GET", "https://api.github.com/orgs/kogosoftwarellc/repos");
-// openRequest.send();
-
-// ******************
-
-
-
-// var calcApp = angular.module('calcApp', []);
-
-// calcApp.controller('calcCtrl', function ($scope) {
-//     $scope.leftOperand = "";
-//     $scope.operator = "";
-//     $scope.rightOperand = "";
-//     $scope.answer = "";
-
-
-//     $scope.setOperand = function (operandEntered) {
-//         if ($scope.operator){
-//             $scope.rightOperand += operandEntered;
-//         }
-//         else {
-//             $scope.leftOperand += operandEntered;
-//         };
-//     };
